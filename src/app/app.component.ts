101cc91577db8f15f86af765ee1dfb2b34433ced
nino kristesiashvili <kristesiashvilinino@gmail.com>
2024-12-13 13:43:51 +0400

    add photos and favourites components with basic templates and styles; update app routing and configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule,RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'photos', pathMatch: 'full'
+    },
+    {
+        path: 'photos', loadComponent: () => import('./photos/photos.component').then(m => m.PhotosComponent)
+    },
+    {
+        path: 'favourites', loadComponent: () => import('./favourites/favourites.component').then(m => m.FavouritesComponent)
+    }
+];
