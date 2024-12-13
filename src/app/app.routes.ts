9eb2c2d54dad3f4a1c7281c68394471bcd572aff
nino kristesiashvili <kristesiashvilinino@gmail.com>
2024-12-13 13:49:11 +0400

    enhance photos route to support nested routes for photo details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,12 @@ export const routes: Routes = [
         path: '', redirectTo: 'photos', pathMatch: 'full'
     },
     {
-        path: 'photos', loadComponent: () => import('./photos/photos.component').then(m => m.PhotosComponent)
+        path: 'photos', children: [
+            {path: '', loadComponent: () => import('./photos/photos.component').then(m => m.PhotosComponent)},        
+            {path: ':id', loadComponent: () => import('./photo-page/photo-page.component').then(m => m.PhotoPageComponent)}
+        
+        ]
+       
     },
     {
         path: 'favourites', loadComponent: () => import('./favourites/favourites.component').then(m => m.FavouritesComponent)
